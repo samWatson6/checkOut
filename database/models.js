7fff61643a0cea1f2c646bf86994e34ff708b661
Sam Watson <Sam.Watson3775@gmail.com>
2018-04-05 18:25:06 -0700

    Export insert function

--- database/models.js
+++ database/models.js
@@ -7,16 +7,16 @@ MongoClient.connect(url, function(err, db) {
   db.close();
 });
 
-//create accounts collection
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   var dbo = db.db("checkout");
-//   dbo.createCollection("accounts", function(err, res) {
-//     if (err) throw err;
-//     console.log("Collection created!");
-//     db.close();
-//   });
-// });
+// create accounts collection
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("checkout");
+  dbo.createCollection("accounts", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});
 
 //insert to accounts collection 
 
